9ced23458a3bd3506cb3c1e55c059b6ddefcdb24
artyom1107@mail.ru <Slavahitleru1488>
2023-12-20 20:45:25 +0500

    added post test edit config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,15 @@
 base_url = 'https://splan-stage.samoletgroup.ru'
-cookie = 'sessionid=jdv10yz4hca9jck1kv7itz68ximg0fnc'
+cookie = 'sessionid=0emhzagm9izzmfk2k0e9z9or1gg9df0s;' \
+         'csrftoken=tgh6KSqEOwekiKOXeaOSl07BVCXe9f3JSbPM7C2IwsMu5s4QQnxUBciO3LuPHkso'
+token = 'BZSbY98UfbBvc9kOSSBxQx7fRFPtxM3H0UqRlTKYX79FZRAHu5kz6JisZOm45Rsm'
 project_id = 64
 pk = 130
 structure_id = 21296
 group_id = 52
+
+payload = {
+  "executor": "test",
+  "address": "test",
+  "comment": "test",
+  "project_id": 1988
+}
---
+++ tests/test_post_supply_project.py
@@ -0,0 +1,24 @@
+import requests
+import src.config
+from src.config import base_url, cookie, token
+
+
+# SPROCUR-2944
+def test_get_post_supply_project():
+    url = base_url + "/api/supply-projects/"
+    headers = {"accept": "application/json",
+               "Cookie": cookie,
+               "Content-Type": "application/json",
+               'X-CSRFToken': token,
+               "Referer": "https://splan-stage.samoletgroup.ru/admin/supplies/supplyproject/add/"}
+
+    response = requests.post(url, headers=headers, json=src.config.payload)
+    json_data = response.json()
+
+    if json_data:
+        assert 'id' in json_data, "Missing 'id' field in the first project"
+        assert 'otif' in json_data, "Missing 'project' field in the first project"
+        assert json_data['project']['id'] == src.config.payload['project_id']
+        assert json_data['executor'] == src.config.payload['executor']
+        assert json_data['address'] == src.config.payload['address']
+        assert json_data['comment'] == src.config.payload['comment']

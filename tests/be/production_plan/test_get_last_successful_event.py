1527510865d63bda02b1bd62ed4a9a359cc4ebd2
artyom1107@mail.ru <Slavahitleru1488>
2024-01-23 14:49:06 +0500

    added try except

--- tests/be/production_plan/test_get_last_successful_event.py
+++ tests/be/production_plan/test_get_last_successful_event.py
@@ -11,32 +11,44 @@ def test_get_list_all_file_config():
                'X-CSRFToken': token
                }
     response = requests.get(url, headers=headers)
+
     # Проверки на код ответа
-    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
-    assert response.headers['Content-Type'] == 'application/json; charset=utf-8', "Response is not in JSON format"
+    try:
+        assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+        assert response.headers['Content-Type'] == 'application/json; charset=utf-8', "Response is not in JSON format"
+    except (AssertionError, TypeError):
+        raise
+
     json_data = response.json()
+    # проверяем блок наличия полей в ответе
+    try:
+        if json_data:
+            assert 'id' in json_data, "Missing 'id' field in the first project"
+            assert 'guid' in json_data, "Missing 'guid' field in the first project"
+            assert 'status' in json_data, "Missing 'status' field in the first project"
+            assert 'user_id' in json_data, "Missing 'user_id' field in the first project"
+            assert 'calculation_type' in json_data, "Missing 'calculation_type' field in the first project"
+            assert 'error_description' in json_data, "Missing 'error_description' field in the first project"
+            assert 'results' in json_data, "Missing 'results' field in the first project"
+            assert 'updated' in json_data, "Missing 'updated' field in the first project"
+            assert 'created' in json_data, "Missing 'created' field in the first project"
+    except (AssertionError, TypeError):
+        raise
+    # Проверки на типы полей
+    try:
+        assert isinstance(json_data["id"], int), "'id' should be int type"
+        assert isinstance(json_data["guid"], str), "'guid' should be string type"
+        assert isinstance(json_data["status"], str), "'status' should be string type"
+        assert isinstance(json_data["user_id"], int), "'user_id' should be int type"
+        assert isinstance(json_data["calculation_type"], str), "'calculation_type' should be string type"
+        assert isinstance(json_data["results"], list), "'results' should be list type"
+        assert isinstance(json_data["updated"], str), "'updated' should be string type"
+        assert isinstance(json_data["created"], str), "'created' should be string type"
+    except (AssertionError, TypeError):
+        raise
 
-    if json_data:
-        # Проверки на поля в ответе
-        assert 'id' in json_data, "Missing 'id' field in the first project"
-        assert 'guid' in json_data, "Missing 'guid' field in the first project"
-        assert 'status' in json_data, "Missing 'status' field in the first project"
-        assert 'user_id' in json_data, "Missing 'user_id' field in the first project"
-        assert 'calculation_type' in json_data, "Missing 'calculation_type' field in the first project"
-        assert 'error_description' in json_data, "Missing 'error_description' field in the first project"
-        assert 'results' in json_data, "Missing 'results' field in the first project"
-        assert 'updated' in json_data, "Missing 'updated' field in the first project"
-        assert 'created' in json_data, "Missing 'created' field in the first project"
-
-        # Проверки на типы полей
-        assert isinstance(json_data["id"], int)
-        assert isinstance(json_data["guid"], str)
-        assert isinstance(json_data["status"], str)
-        assert isinstance(json_data["user_id"], int)
-        assert isinstance(json_data["calculation_type"], str)
-        assert isinstance(json_data["results"], list)
-        assert isinstance(json_data["updated"], str)
-        assert isinstance(json_data["created"], str)
-
-        # Проверка, что количество ключей в ответе соответствует ожидаемому
-        assert len(json_data) == expected_keys_count, "Изменение в количестве ключей в ответе"
+    # Проверка, что количество ключей в ответе соответствует ожидаемому
+    try:
+        assert len(json_data) == expected_keys_count, "Extra or missing key"
+    except (AssertionError, TypeError):
+        raise
--- tests/be/production_plan/test_get_list_all_file_config.py
+++ tests/be/production_plan/test_get_list_all_file_config.py
@@ -14,33 +14,56 @@ def test_get_list_all_file_config():
                'X-CSRFToken': token
                }
     response = requests.get(url, headers=headers)
-    # Проверки на код ответа
-    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
-    assert response.headers['Content-Type'] == 'application/json; charset=utf-8', "Response is not in JSON format"
-    json_data = response.json()
-    # Проверки на наличие полей в ответе
-    assert 'results' in json_data, "Missing 'results' field in response"
-    assert 'count' in json_data, "Missing 'count' field in response"
-    assert 'per_page' in json_data, "Missing 'per_page' field in response"
-    assert isinstance(json_data['results'], list), "'Results' is not a list in response"  # проверка что это список
 
-    # Если резалтс не пустой, то идет проверка на наличие обязательных полей в первом элементе списка
-    if json_data['results']:
-        first_project = json_data['results'][0]
-        assert 'id' in first_project, "Missing 'id' field in the first project"
-        assert 'name' in first_project, "Missing 'name' field in the first project"
-        assert 'title' in first_project, "Missing 'title' field in the first project"
-        assert 'file_kind' in first_project, "Missing 'file_kind' field in the first project"
-        assert 'algorithm_type' in first_project, "Missing 'algorithm_type' field in the first project"
-        assert 'is_file_reload' in first_project, "Missing 'is_file_reload' field in the first project"
-        # Проверки на то что переданный алгоритм соответствует алгоритму в ответе
+    # Проверки на код ответа
+    try:
+        assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+        assert response.headers['Content-Type'] == 'application/json; charset=utf-8', "Response is not in JSON format"
+    except (AssertionError, TypeError):
+        raise
+
+    json_data = response.json()
+
+    # Проверки на наличие полей в ответе
+    try:
+        assert 'results' in json_data, "Missing 'results' field in response"
+        assert 'count' in json_data, "Missing 'count' field in response"
+        assert 'per_page' in json_data, "Missing 'per_page' field in response"
+        assert isinstance(json_data['results'], list), "'Results' is not a list in response"
+    except (AssertionError, TypeError):
+        raise
+
+    try:
+        if json_data['results']:
+            first_project = json_data['results'][0]
+            assert 'id' in first_project, "Missing 'id' field in the first project"
+            assert 'name' in first_project, "Missing 'name' field in the first project"
+            assert 'title' in first_project, "Missing 'title' field in the first project"
+            assert 'file_kind' in first_project, "Missing 'file_kind' field in the first project"
+            assert 'algorithm_type' in first_project, "Missing 'algorithm_type' field in the first project"
+            assert 'is_file_reload' in first_project, "Missing 'is_file_reload' field in the first project"
+    except (AssertionError, TypeError):
+        raise
+
+    # Проверки на то что переданный алгоритм соответствует алгоритму в ответе
+    try:
         assert first_project['algorithm_type'] == REDISTRIBUTION_ALGORITHM
-        # Проверки на типы ответов
+    except (AssertionError, TypeError):
+        raise
+
+    # Проверки на типы ответов
+    try:
         assert isinstance(first_project['id'], int)
         assert isinstance(first_project['name'], str)
         assert isinstance(first_project["title"], str)
         assert isinstance(first_project["file_kind"], str)
         assert isinstance(first_project["algorithm_type"], str)
         assert isinstance(first_project["is_file_reload"], bool)
-        # Проверка на количество элементов
+    except (AssertionError, TypeError):
+        raise
+
+    # Проверка на количество элементов
+    try:
         assert len(first_project) == expected_keys_count, "Изменение в количестве ключей в ответе"
+    except (AssertionError, TypeError):
+        raise

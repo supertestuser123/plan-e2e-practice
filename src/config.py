d8a011308bc506f5a347c4cfd62f2434df0ff7e5
artyom1107@mail.ru <Slavahitleru1488>
2024-01-28 22:16:43 +0500

    updated files

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 base_url = 'https://splan-stage.samoletgroup.ru/'
 cookie = 'sessionid=j9uu8ijel6wdfmgykozp6344lh6vnuza;' \
          'csrftoken=jXFZ4zLNEtMFIuwZ32yuSVK2B1iikMgSFqAH6tYqnBos8G3zk3GuF34MEDZB17H9'
-token = 'putPpp4K5Yvf5zNZq97iaa5rgt5UNNjJLXoxrjhnO672vLkzHafiXipbj5Mdu8K0'
+token = 'eU5KJxA0r5CJECvDzspkjYxX4suNSO0pAn0sLrNDadew4O2dQtxk66RH74b6z9rG'
 project_id = 64
 pk = 130
 structure_id = 21296
@@ -26,3 +26,6 @@ NUMBER_OF_FILES_IN_CONFIG = 7
 REDISTRIBUTION_ALGORITHM = 'redistribution'
 PLANNING_ALGORITHM = 'planning'
 SUB_MODE = 'mixed'
+
+TEST_EVENT_GUID = 'c045d6bc-42a4-41fe-b600-6536d9f60066'
+FILE_KIND = 'BLACK_LIST'
---
+++ tests/be/production_plan/test_post_create_or_update_file.py
@@ -0,0 +1,90 @@
+import requests
+from helper.data_type_checker import check_data_type
+from src.config import base_url, cookie, token, NUMBER_OF_FILES_IN_CONFIG, REDISTRIBUTION_ALGORITHM, SUB_MODE, \
+    FILE_KIND, TEST_EVENT_GUID
+import os
+
+
+def test_post_get_or_create():
+    url = f"{base_url}api/calculation-production-plan/files/{TEST_EVENT_GUID}/?file_kind={FILE_KIND}&algorithm_type={REDISTRIBUTION_ALGORITHM}"
+
+    headers = {"Cookie": cookie,
+               "accept": 'application/json',
+               'Content-Type': 'multipart/form-data',
+               'X-CSRFToken': token,
+               "Referer": "https://splan-stage.samoletgroup.ru"
+               }
+
+    files = {'upload_file': ('Стоп-лист.xlsx', open('Стоп-лист.xlsx', 'rb'),
+                             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')}
+    #
+    #
+    # #
+    response = requests.post(url, headers=headers, files=files)
+    print(response.status_code)
+    print(response.text)
+    # # Проверки на код ответа
+    # try:
+    #     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+    #     assert response.headers['Content-Type'] == 'application/json; charset=utf-8', "Response is not in JSON format"
+    # except (AssertionError, TypeError):
+    #     raise
+    #
+    # json_data = response.json()
+    # print(json_data)
+    #
+    # # Проверки на наличие ключей в ответе
+    # try:
+    #     required_fields = ['id', 'guid', 'status', 'user_id', 'calculation_type', 'sub_mode', 'file_configs', 'files',
+    #                        'error_description', 'results', 'updated', 'created']
+    #     if json_data:
+    #         for field in required_fields:
+    #             assert field in json_data, f"Missing '{field}' field"
+    # except (AssertionError, TypeError):
+    #     raise
+    #
+    # # Проверки на то что переданный алгоритм и подтип соответствует алгоритму в запросе
+    # try:
+    #     assert json_data['calculation_type'] == REDISTRIBUTION_ALGORITHM
+    #     assert json_data['sub_mode'] == SUB_MODE
+    # except (AssertionError, TypeError):
+    #     raise
+    #
+    # # Проверки на типы ключей
+    # try:
+    #     type_checks = {
+    #         'id': int,
+    #         'guid': str,
+    #         'status': str,
+    #         'user_id': int,
+    #         'calculation_type': str,
+    #         'sub_mode': str,
+    #         'file_configs': list,
+    #         'updated': str,
+    #         'created': str
+    #     }
+    #     for field, expected_type in type_checks.items():
+    #         assert isinstance(json_data.get(field),
+    #                           expected_type), f"'{field}' should be {expected_type} type"
+    #     try:
+    #         check_data_type(json_data, "files", list)
+    #         check_data_type(json_data, "error_description", dict)
+    #         check_data_type(json_data, "results", list)
+    #     except AssertionError as e:
+    #         print(e)
+    #         raise
+    # except (AssertionError, TypeError):
+    #     raise
+    #
+    # # Проверка на количество ключей в ответе
+    # try:
+    #     EXPECTED_KEYS_COUNT = 12
+    #     assert len(json_data) == EXPECTED_KEYS_COUNT, "Изменение в количестве ключей в ответе"
+    # except (AssertionError, TypeError):
+    #     raise
+    #
+    # # Проверка на количество конфигов в file_configs
+    # try:
+    #     assert len(json_data["file_configs"]) == NUMBER_OF_FILES_IN_CONFIG, "Изменение в количестве ключей в ответе"
+    # except (AssertionError, TypeError):
+    #     raise

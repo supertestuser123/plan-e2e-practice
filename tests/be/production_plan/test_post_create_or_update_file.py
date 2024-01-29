78fc631714dcf55b4ae6357d05b1f0de5758d764
artyom1107@mail.ru <Slavahitleru1488>
2024-01-29 15:36:59 +0500

    added full test + lint fixes

--- tests/be/production_plan/test_post_create_or_update_file.py
+++ tests/be/production_plan/test_post_create_or_update_file.py
@@ -1,5 +1,3 @@
-import os
-
 import requests
 from src.config import base_url, cookie, token, REDISTRIBUTION_ALGORITHM, FILE_KIND, TEST_EVENT_GUID
 
@@ -17,5 +15,53 @@ def test_post_get_or_create():
                              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')}
 
     response = requests.post(url, headers=headers, files=files)
-    print(response.status_code)
-    print(response.text)
+
+    try:
+        assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+        assert response.headers['Content-Type'] == 'application/json; charset=utf-8', "Response is not in JSON format"
+    except (AssertionError, TypeError):
+        raise
+
+    json_data = response.json()
+
+    # Проверки на наличие ключей в ответе
+    try:
+        required_fields = ['id', 'name', 'title', 'file_kind', 'event_id', 'event_guid', 'url', 'created',
+                           'updated']
+        if json_data:
+            for field in required_fields:
+                assert field in json_data, f"Missing '{field}' field"
+    except (AssertionError, TypeError):
+        raise
+
+    # Проверки на то что переданный алгоритм и подтип соответствует алгоритму в запросе
+    try:
+        assert json_data['event_guid'] == TEST_EVENT_GUID
+        assert json_data['file_kind'] == FILE_KIND
+    except (AssertionError, TypeError):
+        raise
+
+    # Проверки на типы ключей
+    try:
+        type_checks = {
+            'id': int,
+            'name': str,
+            'title': str,
+            'event_id': int,
+            'event_guid': str,
+            'url': str,
+            'created': str,
+            'updated': str,
+        }
+        for field, expected_type in type_checks.items():
+            assert isinstance(json_data.get(field),
+                              expected_type), f"'{field}' should be {expected_type} type"
+    except (AssertionError, TypeError):
+        raise
+
+    # Проверка на количество ключей в ответе
+    try:
+        EXPECTED_KEYS_COUNT = 9
+        assert len(json_data) == EXPECTED_KEYS_COUNT, "Изменение в количестве ключей в ответе"
+    except (AssertionError, TypeError):
+        raise
